5ebfa781abab33518c54513586dbc0866eb5849e
Leonard Laput <online.lorno@gmail.com>
2022-08-04 09:20:50 +0800

    🔧 Config to use .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { CustomersController } from './customers/customers.controller'
 import { CustomersService } from './customers/customers.service'
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController, CustomersController],
   providers: [AppService, CustomersService],
 })
